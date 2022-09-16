bb3c5ec79c5630559597988615bbc180d57ed6e4
Gabriel <105824688+Custodio97@users.noreply.github.com>
2022-09-15 21:59:45 -0300

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,21 @@
+import HomeHeader from "./HomeHedaer";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NTFCard from './NTFCard'
+import { RectButton, CircleButton } from './Button'
+import { EThPrice, NFTTitle, EndDate, People, SubInfo } from './SubInfo'
+import DetailsBid from './DetailsBid'
+import DetailsDesc from './DetailsDesc'
+export {
+    HomeHeader,
+    NTFCard,
+    FocusedStatusBar,
+    RectButton,
+    CircleButton,
+    EThPrice,
+    NFTTitle,
+    EndDate,
+    People,
+    SubInfo,
+    DetailsBid,
+    DetailsDesc,
+};
